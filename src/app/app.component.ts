a279ed1c1294ebca8c3ae9ab99a443da44ea6898
Shashank Pratheepkumar <Shashanksanthosh19@gmail.com>
2024-11-13 17:33:11 +0530

    Added the template and function for add tasks

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,30 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormsModule, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'my-todo-list';
+
+  public items: string[] = [];
+  public newTask: string = '';
+
+  public addToList() {
+    if (this.newTask.trim()) {
+      this.items.push(this.newTask);
+      this.newTask = '';
+    }
+  }
+
+  public deleteTask(index: number) {
+    this.items.splice(index, 1);
+  }
+  
 }
